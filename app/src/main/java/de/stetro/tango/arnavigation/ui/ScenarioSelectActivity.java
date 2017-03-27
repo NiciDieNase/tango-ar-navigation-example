d98d7e06b36890c6cb61aa04430a91591a15c82e
Felix Bürkle <f.buerkle@inovex.de>
2017-03-27 15:37:55 +0200

    add Scenario-Selection Activity

---
+++ app/src/main/java/de/stetro/tango/arnavigation/ui/ScenarioSelectActivity.java
@@ -0,0 +1,19 @@
+package de.stetro.tango.arnavigation.ui;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import de.stetro.tango.arnavigation.R;
+
+/**
+ * Created by felix on 27/03/17.
+ */
+
+public class ScenarioSelectActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.scenario_select);
+    }
+}
